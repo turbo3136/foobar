71e9f87393c631d50b381cc59dd6c8a0c39a6ec8
turbo3136 <turbo3136@gmail.com>
2017-10-19 00:02:32 -0400

    starting notes

--- brent/level_3/tree_search.py
+++ brent/level_3/tree_search.py
@@ -1,1 +1,7 @@
 # use a tree search algorithm to find the best path through the maze
+"""
+I think the best way to do this is going to be
+using A* to find open spaces, but also look for
+connections through one wall. Also look at the
+shortest available paths between two points
+"""
