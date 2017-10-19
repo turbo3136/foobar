aba1b4b78950fcace085fae46ae5f18f4d3a12c5
Brent <turbo3136@gmail.com>
2017-10-19 13:18:41 -0400

    search tree logic to use

--- brent/level_3/tree_search.py
+++ brent/level_3/tree_search.py
@@ -1,1 +1,7 @@
 # use a tree search algorithm to find the best path through the maze
+"""
+Okay, so what I really want to do is use a Breadth First Search to find the
+shortest path through the maze, but use a two-level queue in order to track
+whether the cell has been visited by a path that has already removed a wall
+or if it's been visited by a path that hasn't removed a wall.
+"""
