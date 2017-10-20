a679beae262b3e602993ea72e6258f3f451b42df
Brent <turbo3136@gmail.com>
2017-10-20 16:11:42 -0400

    but it's too slow...

--- brent/level_3/tree_search.py
+++ brent/level_3/tree_search.py
@@ -1,3 +1,6 @@
+import time
+start_time = time.time()
+
 """
     So... we're going to use a breadth first search algorithm, which
     takes one step in each possible direction before going to the next step.
@@ -10,11 +13,11 @@
     cell that's a dead end for a path that's already removed one wall, but
     it's not a dead end for a path that has removed no walls.
 """
-def answer(map):
+def answer(maze):
     # store a few things to start
     start = [0, 0]
-    end = [len(map) - 1, len(map[0]) - 1]   # ending position
-    size = [len(map), len(map[0])]          # map size for loops
+    end = [len(maze) - 1, len(maze[0]) - 1]   # ending position
+    size = [len(maze), len(maze[0])]          # maze size for loops
     x = 0
     y = 1
 
@@ -26,18 +29,17 @@ def answer(map):
     q_step = [0]
 
 
-    # create a matrix the size of map and initialize with 0's
+    # create a matrix the size of maze and initialize with 0's
     # we'll use it to track if we've been to a node and if it's with a path thru a wall
     visited = [[0 for j in range(size[y])] for i in range(size[x])]
 
     """here's the beginning of the loop"""
-    n = 0
-    while n < 100:
+    while q:
 
         # start with the current node and set visited[node[x]][node[y]] = 2 if is_wall_path = 0, else 1
         # Look to see which adjacent nodes are open, based on whether we've been thru a wall or not.
         # Put the open nodes in the queue
-        # (it's open if is_wall_path == 0 or map[node[x]][node[y]] == 0)
+        # (it's open if is_wall_path == 0 or maze[node[x]][node[y]] == 0)
         node = q.pop(0)
         is_wall_path = q_wall.pop(0)
         step = q_step.pop(0)
@@ -56,67 +58,29 @@ def answer(map):
             # if we're not moving past the left or right edge, look at the cell
             if 0 <= node[x] + i < size[x]:
                 new_node = [node[x] + i, node[y]]
-                print 'step ' + str(n) + 'x:\t' + str(new_node) + '\t' + str(map[new_node[x]][new_node[y]])
 
-                # if is_wall_path + map + visited < 2, then we'll put that in the queue
-                # and set q_wall.append(is_wall_path + map)
-                if is_wall_path + map[new_node[x]][new_node[y]] + visited[new_node[x]][new_node[y]] < 2:
+                # if is_wall_path + maze + visited < 2, then we'll put that in the queue
+                # and set q_wall.append(is_wall_path + maze)
+                if is_wall_path + maze[new_node[x]][new_node[y]] + visited[new_node[x]][new_node[y]] < 2:
                     q.append(new_node)
-                    q_wall.append(is_wall_path + map[new_node[x]][new_node[y]])
+                    q_wall.append(is_wall_path + maze[new_node[x]][new_node[y]])
                     q_step.append(step + 1)
-                    print '\t\t\t' + str(q)
-                    print '\t\t\t' + str(q_wall)
-                    print '\t\t\t' + str(q_step)
 
             # if we're not moving past the top or bottom edge, look at the cell
             if 0 <= node[y] + i < size[y]:
                 new_node = [node[x], node[y] + i]
-                print 'step ' + str(n) + 'y:\t' + str(new_node) + '\t' + str(map[new_node[x]][new_node[y]])
 
                 # same logic as above
-                if is_wall_path + map[new_node[x]][new_node[y]] + visited[new_node[x]][new_node[y]] < 2:
+                if is_wall_path + maze[new_node[x]][new_node[y]] + visited[new_node[x]][new_node[y]] < 2:
                     q.append(new_node)
-                    q_wall.append(is_wall_path + map[new_node[x]][new_node[y]])
+                    q_wall.append(is_wall_path + maze[new_node[x]][new_node[y]])
                     q_step.append(step + 1)
-                    print '\t\t\t' + str(q)
-                    print '\t\t\t' + str(q_wall)
-                    print '\t\t\t' + str(q_step)
 
-        # print visited for visualization
-        for j in range(size[y]):
-            s = ''
-            for i in range(size[x]):
-                s += str(visited[i][j]) + '\t'
-            print s
-        print
-
-        n += 1
     """and the end of the loop"""
 
 
-    # print visited for visualization
-    for j in range(size[y]):
-        s = ''
-        for i in range(size[x]):
-            s += str(visited[i][j]) + '\t'
-        print s
-    print
-
-    # print visited_wall for visualization
-    # for j in range(size[y]):
-    #     s = ''
-    #     for i in range(size[x]):
-    #         s += str(visited_wall[i][j]) + '\t'
-    #     print s
-    # print
-
-    # print the map for visualization
-    for j in range(size[y]):
-        s = ''
-        for i in range(size[x]):
-            s += str(map[i][j]) + '\t'
-        print s
-
-
 # print answer([[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]])
-print answer([[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]])
+# print answer([[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]])
+print answer([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+print("--- %s seconds ---" % (time.time() - start_time))
