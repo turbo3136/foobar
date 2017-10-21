a2d29f8a63ec067b1cd1e1730b73d9377f16db4f
turbo3136 <turbo3136@gmail.com>
2017-10-21 15:56:53 -0400

    start of the a star

--- brent/level_3/tree_search.py
+++ brent/level_3/tree_search.py
@@ -1,7 +1,37 @@
-# use a tree search algorithm to find the best path through the maze
-"""
-I think the best way to do this is going to be
-using A* to find open spaces, but also look for
-connections through one wall. Also look at the
-shortest available paths between two points
-"""
+# use A* tree search algorithm to find the best path through the maze
+
+
+# create a cost function that finds the distance between
+# two points, assuming there's no wall
+def distance_guess(x1, y1, x2, y2):
+    ret = abs(x2 - x1) + abs(y2 - y1)
+    return ret
+
+
+def path_length(path):
+    return len(path)
+
+
+def answer(maze):
+    # start with some values
+    start = [0, 0]
+    end = [len(maze) - 1, len(maze[0]) - 1]
+    size = [len(maze), len(maze[0])]
+    x = 0
+    y = 1
+
+    # the queue
+    q = [start]
+    q_wall = [0]
+    q_step = [0]
+    q_weight = distance_guess(0, 0, end[x], end[y])
+    print q_weight
+    # the visited
+    visited = [[0 for j in range(size[y])] for i in range(size[x])]
+
+
+print answer([[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]])
+# print answer([[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]])
+# print answer([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+
