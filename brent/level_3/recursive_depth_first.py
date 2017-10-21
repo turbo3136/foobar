4289f4d8a732558a417670c9c1fe87ec9911f72c
turbo3136 <turbo3136@gmail.com>
2017-10-20 22:53:18 -0400

    recursive depth first with wall removal

--- brent/level_3/recursive_depth_first.py
+++ brent/level_3/recursive_depth_first.py
@@ -1,54 +1,61 @@
 import time
 start_time = time.time()
 
-
-def valid_step(maze, x, y, visited):
+# valid_step returns -1 for invalid steps or the new value for is_wall_path if valid
+def valid_step(maze, x, y, visited, is_wall_path):
     max_x = len(maze) - 1
     max_y = len(maze[0]) - 1
 
     if x > max_x or y > max_y or x < 0 or y < 0:
-        return False
+        return -1
 
-    elif visited[x][y] == 1:
-        return False
+    elif is_wall_path + maze[x][y] + visited[x][y] < 2:
+        return is_wall_path + maze[x][y]
 
-    elif maze[x][y] == 1:
-        return False
-
-    return True
+    else:
+        return -1
 
 
-def dfs(maze, x_pos, y_pos, visited):
+def dfs(maze, x_pos, y_pos, visited, is_wall_path):
     end_x = len(maze) - 1
     end_y = len(maze[0]) - 1
-    visited[x_pos][y_pos] = 1
 
     # if we're one away from the end of the maze, return 2
     if (x_pos == end_x - 1 and y_pos == end_y) or (x_pos == end_x and y_pos == end_y - 1):
         return 2
 
+    # if we haven't gone thru a wall, set visited = 2, else visited = 1
+    if is_wall_path == 0:
+        visited[x_pos][y_pos] = 2
+    else:
+        visited[x_pos][y_pos] = 1
+
     solution_size = 9999
 
     # East
-    if valid_step(maze, x_pos + 1, y_pos, visited):
-        steps = dfs(maze, x_pos + 1, y_pos, visited)
+    check = valid_step(maze, x_pos + 1, y_pos, visited, is_wall_path)
+    if check > -1:
+        steps = dfs(maze, x_pos + 1, y_pos, visited, check)
         solution_size = steps + 1
 
     # South
-    if valid_step(maze, x_pos, y_pos + 1, visited):
-        steps = dfs(maze, x_pos, y_pos + 1, visited)
+    check = valid_step(maze, x_pos, y_pos + 1, visited, is_wall_path)
+    if check > -1:
+        steps = dfs(maze, x_pos, y_pos + 1, visited, check)
         if steps + 1 < solution_size:
             solution_size = steps + 1
 
     # West
-    if valid_step(maze, x_pos - 1, y_pos, visited):
-        steps = dfs(maze, x_pos - 1, y_pos, visited)
+    check = valid_step(maze, x_pos - 1, y_pos, visited, is_wall_path)
+    if check > -1:
+        steps = dfs(maze, x_pos - 1, y_pos, visited, check)
         if steps + 1 < solution_size:
             solution_size = steps + 1
 
     # North
-    if valid_step(maze, x_pos, y_pos - 1, visited):
-        steps = dfs(maze, x_pos, y_pos - 1, visited)
+    check = valid_step(maze, x_pos, y_pos - 1, visited, is_wall_path)
+    if check > -1:
+        steps = dfs(maze, x_pos, y_pos - 1, visited, check)
         if steps + 1 < solution_size:
             solution_size = steps + 1
 
@@ -58,16 +65,18 @@ def dfs(maze, x_pos, y_pos, visited):
 def answer(maze):
     start_x = 0
     start_y = 0
+    is_wall_path = 0
 
     visited = [[0 for j in range(len(maze[0]))] for i in range(len(maze))]
 
-    ret = dfs(maze, start_x, start_y, visited)
+    ret = dfs(maze, start_x, start_y, visited, is_wall_path)
 
     return ret
 
 
 # print answer([[0, 0, 0, 0], [0, 0, 0, 0]])
-# print answer([[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]])
-print answer([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+print answer([[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]])
+# print answer([[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]])
+# print answer([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
 print("--- %s seconds ---" % (time.time() - start_time))
